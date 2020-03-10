dc44a151fc4d50dcb73349d45b1d8527d3696bb4
sken <sken77@pm.me>
2020-03-10 13:29:54 -0700

    fix type inference issue

--- core/src/main/scala/org/scash/core/number/Uint32B.scala
+++ core/src/main/scala/org/scash/core/number/Uint32B.scala
@@ -5,6 +5,7 @@ import scodec.Attempt.Successful
 import scodec.Codec
 import scodec.codecs._
 import org.scash.core.typeclass._
+
 case class Uint32B(private[core] val num: BigInt)
 
 object Uint32B {
@@ -15,5 +16,5 @@ object Uint32B {
   )
 
   implicit val uint32Numeric: CNumeric[Uint32B] =
-    CNumeric[Uint32B](_.num, Uint32B(_), 0xFFFFFFFFL)
+    CNumeric[Uint32B](0xFFFFFFFFL)(_.num, Uint32B(_))
 }
