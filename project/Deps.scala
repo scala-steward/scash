6bd241f990acbb7f15233136daf81ad7d8ddb6f9
sken <sken77@pm.me>
2020-03-20 13:57:00 -0700

    Revert "remove secp256k1/jni dependency and pull it from the repo"
    This reverts commit 04115f3a1c4ed41551283b4e3880f3ecbdb3d298.

--- project/Deps.scala
+++ project/Deps.scala
@@ -18,6 +18,7 @@ object Deps {
     val junitv          = "0.11"
     val zioV            = "1.0.0-RC18-2"
     val typesafeConfigV = "1.4.0"
+    val nativeLoaderV   = "2.3.4"
     val scalaTestPlus   = "3.1.1.1"
     val asyncNewScalaV  = "0.10.0"
     val akkaActorV      = akkaStreamv
@@ -26,36 +27,38 @@ object Deps {
   }
 
   object Compile {
-    val bouncycastle   = "org.bouncycastle"             % "bcprov-jdk15on"                 % V.bouncyCastle
-    val scodecbits     = "org.scodec"                   %% "scodec-bits"                   % V.scodecbitsv
-    val scodec         = "org.scodec"                   %% "scodec-core"                   % V.scodecv
-    val slf4j          = "org.slf4j"                    % "slf4j-api"                      % V.slf4j % "provided"
-    val zeromq         = "org.zeromq"                   % "jeromq"                         % V.zeromq
-    val akkaHttp       = "com.typesafe.akka"            %% "akka-http"                     % V.akkav
-    val akkaStream     = "com.typesafe.akka"            %% "akka-stream"                   % V.akkaStreamv
-    val playJson       = "com.typesafe.play"            %% "play-json"                     % V.playv
-    val scalaz         = "org.scalaz"                   %% "scalaz-core"                   % V.scalazv withSources () withJavadoc ()
-    val zio            = "dev.zio"                      %% "zio"                           % V.zioV withSources () withJavadoc ()
-    val typesafeConfig = "com.typesafe"                 % "config"                         % V.typesafeConfigV withSources () withJavadoc ()
-    val secp256k1jni   = "org.scash"                    %% "secp256k1jni"                  % V.secp256k1jni
-    val sttp           = "com.softwaremill.sttp.client" %% "core"                          % V.sttpV
-    val sttpZio        = "com.softwaremill.sttp.client" %% "async-http-client-backend-zio" % V.sttpV
-    val sttpJson       = "com.softwaremill.sttp.client" %% "play-json"                     % V.sttpV
+    val bouncycastle   = "org.bouncycastle"  % "bcprov-jdk15on" % V.bouncyCastle
+    val scodecbits     = "org.scodec"        %% "scodec-bits"   % V.scodecbitsv
+    val scodec         = "org.scodec"        %% "scodec-core"   % V.scodecv
+    val slf4j          = "org.slf4j"         % "slf4j-api"      % V.slf4j % "provided"
+    val zeromq         = "org.zeromq"        % "jeromq"         % V.zeromq
+    val akkaHttp       = "com.typesafe.akka" %% "akka-http"     % V.akkav
+    val akkaStream     = "com.typesafe.akka" %% "akka-stream"   % V.akkaStreamv
+    val playJson       = "com.typesafe.play" %% "play-json"     % V.playv
+    val scalaz         = "org.scalaz"        %% "scalaz-core"   % V.scalazv withSources () withJavadoc ()
+    val zio            = "dev.zio"           %% "zio"           % V.zioV withSources () withJavadoc ()
+    val typesafeConfig = "com.typesafe"      % "config"         % V.typesafeConfigV withSources () withJavadoc ()
+    //for loading secp256k1 natively
+    val nativeLoader = "org.scijava"                  % "native-lib-loader"              % V.nativeLoaderV withSources () withJavadoc ()
+    val sttp         = "com.softwaremill.sttp.client" %% "core"                          % V.sttpV
+    val sttpZio      = "com.softwaremill.sttp.client" %% "async-http-client-backend-zio" % V.sttpV
+    val sttpJson     = "com.softwaremill.sttp.client" %% "play-json"                     % V.sttpV
   }
 
   object Test {
-    val newAsync      = "org.scala-lang.modules" %% "scala-async" % V.asyncNewScalaV % "test" withSources () withJavadoc ()
-    val bitcoinj      = ("org.bitcoinj" % "bitcoinj-core" % "0.14.4" % "test").exclude("org.slf4j", "slf4j-api")
-    val logback       = "ch.qos.logback" % "logback-classic" % V.logback % "test"
-    val scalacheck    = "org.scalacheck" %% "scalacheck" % V.scalacheck % "test" withSources () withJavadoc ()
-    val scalaTest     = "org.scalatest" %% "scalatest" % V.scalaTest % "test"
-    val scalaTestPlus = "org.scalatestplus" %% "scalacheck-1-14" % V.scalaTestPlus
-    val spray         = "io.spray" %% "spray-json" % V.spray % "test"
-    val akkaHttp      = "com.typesafe.akka" %% "akka-http-testkit" % V.akkav % "test"
-    val akkaStream    = "com.typesafe.akka" %% "akka-stream-testkit" % V.akkaStreamv % "test"
-    val akkaTestkit   = "com.typesafe.akka" %% "akka-testkit" % V.akkaActorV withSources () withJavadoc ()
-    val zioTest       = "dev.zio" %% "zio-test" % V.zioV % "test"
-    val zioTestsbt    = "dev.zio" %% "zio-test-sbt" % V.zioV % "test"
+    val newAsync       = "org.scala-lang.modules" %% "scala-async" % V.asyncNewScalaV % "test" withSources () withJavadoc ()
+    val bitcoinj       = ("org.bitcoinj" % "bitcoinj-core" % "0.14.4" % "test").exclude("org.slf4j", "slf4j-api")
+    val junitInterface = "com.novocode" % "junit-interface" % V.junitv % "test"
+    val logback        = "ch.qos.logback" % "logback-classic" % V.logback % "test"
+    val scalacheck     = "org.scalacheck" %% "scalacheck" % V.scalacheck % "test" withSources () withJavadoc ()
+    val scalaTest      = "org.scalatest" %% "scalatest" % V.scalaTest % "test"
+    val scalaTestPlus  = "org.scalatestplus" %% "scalacheck-1-14" % V.scalaTestPlus
+    val spray          = "io.spray" %% "spray-json" % V.spray % "test"
+    val akkaHttp       = "com.typesafe.akka" %% "akka-http-testkit" % V.akkav % "test"
+    val akkaStream     = "com.typesafe.akka" %% "akka-stream-testkit" % V.akkaStreamv % "test"
+    val akkaTestkit    = "com.typesafe.akka" %% "akka-testkit" % V.akkaActorV withSources () withJavadoc ()
+    val zioTest        = "dev.zio" %% "zio-test" % V.zioV % "test"
+    val zioTestsbt     = "dev.zio" %% "zio-test-sbt" % V.zioV % "test"
   }
 
   val core = List(
@@ -63,8 +66,12 @@ object Deps {
     Compile.scodecbits,
     Compile.scodec,
     Compile.slf4j,
-    Compile.scalaz,
-    Compile.secp256k1jni
+    Compile.scalaz
+  )
+
+  val secp256k1jni = List(
+    Compile.nativeLoader,
+    Test.junitInterface
   )
 
   val testkit = List(
--- project/Projects.scala
+++ project/Projects.scala
@@ -2,5 +2,6 @@ import sbt._
 
 object Projects {
   val core = project in file("..") / "core"
-  val rpc  = project in file("..") / "rpc"
+  val rpc = project in file("..") / "rpc"
+  val secp256k1jni = project in file("..") / "secp256k1jni "
 }
